1a0649b7fda8fd4adf278f0fc08bccf84cd14775
MJWORKS <mjworks2020@outlook.com>
2022-09-16 16:32:21 -0300

    Added new changes in the AuthUserService

--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,3 +1,4 @@
+import { compare } from "bcryptjs";
 import prismaCLient from "../../prisma/index";
 
 interface AuthUserRequest {
@@ -7,7 +8,26 @@ interface AuthUserRequest {
 
 class AuthUserService {
   async execute({ email, password }: AuthUserRequest) {
-    console.log(email, password);
+    const user = await prismaCLient.user.findFirst({
+      where: {
+        email: email,
+      },
+      include: {
+        subscriptions: true,
+      },
+    });
+
+    if (!user) {
+      throw new Error("Email/password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user?.password);
+    if (!passwordMatch) {
+      throw new Error("Email/password incorrect");
+    }
+
+    // Gerar um token JWT
+
     return { ok: true };
   }
 }
