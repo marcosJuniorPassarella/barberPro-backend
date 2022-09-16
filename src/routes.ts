bc9fc3e91f02478b7bec76a6532630afaa620e96
MJWORKS <mjworks2020@outlook.com>
2022-09-16 18:49:42 -0300

    Added /me route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response } from "express";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
 
 const router = Router();
 
 // --- USER ROUTES ---
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
+router.get("/me", new DetailUserController().handle);
 
 export { router };
