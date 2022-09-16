a93e0116bf42850d855e2dafeb1836903e26a851
MJWORKS <mjworks2020@outlook.com>
2022-09-16 15:54:21 -0300

    Added initial implementation of Auth

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router, Request, Response } from "express";
+import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 
 const router = Router();
 
 // --- USER ROUTES ---
 router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
 
 export { router };
--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,1 +1,15 @@
-import prismaCLient from '../../prisma/index';
+import prismaCLient from "../../prisma/index";
+
+interface AuthUserRequest {
+  email: string;
+  password: string;
+}
+
+class AuthUserService {
+  async execute({ email, password }: AuthUserRequest) {
+    console.log(email, password);
+    return { ok: true };
+  }
+}
+
+export { AuthUserService };
