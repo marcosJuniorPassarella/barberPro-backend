d68bee4f6b1db5499d6c5899a6712f8aa73f358e
MJWORKS <mjworks2020@outlook.com>
2022-09-19 16:07:42 -0300

    Added updates in the DetaiUser route

--- src/services/user/DetailUserService.ts
+++ src/services/user/DetailUserService.ts
@@ -1,8 +1,26 @@
 import prismaCLient from "../../prisma/index";
 
 class UserDetailService {
-  async execute() {
-    return { ok: true };
+  async execute(user_id: string) {
+    const user = await prismaCLient.user.findFirst({
+      where: {
+        id: user_id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        endereco: true,
+        subscriptions: {
+          select: {
+            id: true,
+            priceId: true,
+            status: true,
+          },
+        },
+      },
+    });
+    return user;
   }
 }
 
