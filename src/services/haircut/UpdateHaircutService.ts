91adea0021181116cb0acf62ef2c3fba826b2a2d
MJWORKS <mjworks2020@outlook.com>
2022-09-19 19:22:59 -0300

    Created UpdateHaircutService

---
+++ src/services/haircut/UpdateHaircutService.ts
@@ -0,0 +1,33 @@
+import prismaCLient from "../../prisma/index";
+interface HaircutRequest {
+  user_id: string;
+  haircut_id: string;
+  name: string;
+  price: number;
+  status: boolean | string;
+}
+
+class UpdateHaircutService {
+  async handle({
+    user_id,
+    haircut_id,
+    name,
+    price,
+    status = true,
+  }: HaircutRequest) {
+    const haircut = await prismaCLient.haircut.update({
+      where: {
+        id: haircut_id,
+      },
+      data: {
+        name: name,
+        price: price,
+        status: status === true ? true : false,
+      },
+    });
+
+    return haircut;
+  }
+}
+
+export { UpdateHaircutService };
