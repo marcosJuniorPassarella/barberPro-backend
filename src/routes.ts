949e34b334a5192d9dd7f14dcf8a48580b244538
MJWORKS <mjworks2020@outlook.com>
2022-09-19 18:35:58 -0300

    Added CreateHaircutController and your route

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from "express";
+import { CreateHaircutController } from "./controllers/haircut/CreateHaircutController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
@@ -12,5 +13,8 @@ router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
 router.put("/users", isAuthenticated, new UpdateUserController().handle);
+
+// ---HAIRCUT ROUTES ---
+router.post("/haircut", isAuthenticated, new CreateHaircutController().handle);
 
 export { router };
