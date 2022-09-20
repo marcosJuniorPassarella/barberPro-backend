4a348fdf68224aedddfd272951a0faf32119130d
MJWORKS <mjworks2020@outlook.com>
2022-09-19 21:34:03 -0300

    Created ListScheduleController and your route

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { CreateHaircutController } from "./controllers/haircut/CreateHaircutCont
 import { DetailHaircutController } from "./controllers/haircut/DetailHaircutController";
 import { ListHaircutController } from "./controllers/haircut/ListHaircutController";
 import { UpdateHaircutController } from "./controllers/haircut/UpdateHaircutController";
+import { ListScheduleController } from "./controllers/schedule/ListScheduleController";
 import { NewScheduleController } from "./controllers/schedule/NewScheduleController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
@@ -42,5 +43,6 @@ router.get(
 
 // --- SCHEDULE - SERVICE ROUTES
 router.post("/schedule", isAuthenticated, new NewScheduleController().handle);
+router.get("/schedule", isAuthenticated, new ListScheduleController().handle);
 
 export { router };
