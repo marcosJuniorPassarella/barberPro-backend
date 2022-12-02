29de85c0cb870569d08d00321c19ca0fd499866b
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-02 19:51:55 -0300

    Criado serviço para realizar pagamento assinatura

--- src/services/subscription/SubscribeService.ts
+++ src/services/subscription/SubscribeService.ts
@@ -1,4 +1,6 @@
+import Stripe from 'stripe';
 import prismaCLient from '../../prisma/index';
+import { stripe } from '../../utils/stripe';
 
 interface SubscribeRequest {
     user_id: string
@@ -6,7 +8,52 @@ interface SubscribeRequest {
 
 class SubscribeService {
     async execute({ user_id }: SubscribeRequest) {
-        return user_id;
+        const stripeConfig = stripe;
+        // Buscar o usuário e cadastrar ele no STRIPE caso não tenha cadastrado
+        const findUser = await prismaCLient.user.findFirst({
+            where: {
+                id: user_id
+            }
+        })
+
+        let customerId = findUser?.stripe_customer_id;
+
+        if (!customerId) {
+            // Caso não tenha customerId criamos lá no Stripe
+            const stripeCustomer = await stripeConfig.customers.create({
+                email: findUser?.email
+            })
+
+            await prismaCLient.user.update({
+                where: {
+                    id: user_id
+                },
+                data: {
+                    stripe_customer_id: stripeCustomer?.id
+                }
+            })
+
+            customerId = stripeCustomer?.id
+        }
+
+        // Inicializar o nosso checkout de pagamento
+        const stripeCheckoutSession = await stripeConfig.checkout.sessions.create({
+            customer: customerId,
+            payment_method_types: ['card'],
+            billing_address_collection: 'required',
+            line_items: [
+                {
+                    price: process.env.STRIPE_PRICE,
+                    quantity: 1
+                }
+            ],
+            mode: 'subscription',
+            allow_promotion_codes: true,
+            success_url: process.env.STRIPE_SUCESS_URL,
+            cancel_url: process.env.STRIPE_CANCEL_URL
+        })
+
+        return { sessionId: stripeCheckoutSession?.id }
     }
 }
 
